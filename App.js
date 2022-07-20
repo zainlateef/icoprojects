83e92e7ed4e6a7ce1823923a9d2f0922369bb6a0
Samad Valiani <Svaliani1@outlook.com>
2022-07-19 20:42:22 -0400

    Buttons is being fixed but this is for nav bar upd

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
+import * as React from 'react'; //For the nav function
+import { NavigationContainer } from '@react-navigation/native'; //Nav functionality
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import botbutton from "/Users/Svaliani/icoproject1/botbuttons.js"; //To get the bottom nav buttons
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+        <botbutton/>
+      </View>
+    </NavigationContainer>
   );
 }
 
