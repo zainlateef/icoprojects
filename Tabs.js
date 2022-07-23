10ce68e2fe988e03f47321423e19304fd6f9f1eb
Samad Valiani <Svaliani1@outlook.com>
2022-07-22 21:33:45 -0400

    Image on buttons

--- Tabs.js
+++ Tabs.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import { Image, View, Text } from 'react-native';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import EventScreen from "./screens/EventScreen";
 import QiblaScreen from "./screens/QiblaScreen";
@@ -10,8 +10,39 @@ const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
     return(
-        <Tab.Navigator>
-            <Tab.Screen name="events" component={EventScreen}/>
+        <Tab.Navigator
+            tabBarOptions={{
+                showLabel: false,
+                style: {
+                    position: 'absolute',
+                    bottom: 25,
+                    left: 20,
+                    right: 20,
+                    elevation: 0,
+                    backgrounColor: '#ffffff',
+                    borderRadius: 15,
+                    height: 90,
+                }
+            }}
+        >
+            <Tab.Screen name="events" component={EventScreen} options={{
+                tabBarIcon: ({focused}) => (
+                    <View style={{alignItems: 'center', justifyContent: 'center', top: 10}}>
+                        <Image
+                            source={require('')} //insert the image path when image is made
+                            resizeMode='contain'
+                            style={{
+                                width: 25,
+                                height: 25,
+
+                            }}
+                        />
+                        <Text style={{color: '#000000', fontSize: 12}}>Events</Text>
+                    </View>
+                )
+            
+        }}
+            />
             <Tab.Screen name="qibla" component={QiblaScreen}/>
             <Tab.Screen name="prayerTimes" component={PrayerTimeScreen}/>
             <Tab.Screen name="donate" component={DonationScreen}/>    
